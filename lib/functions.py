51a441f051b0e2fd98edd5b27a842461e22b0f2f
good-lancer <developer613@sourceplay.com>
2020-09-28 15:51:31 +0100

    reply-to value seems malformed #134

--- lib/functions.py
+++ lib/functions.py
@@ -19,7 +19,7 @@ def send_mail_with_html(subject, html_message, to_email, reply_to, from_email = 
         subject=subject,
         from_email=from_email,
         to=to, 
-        reply_to=[reply_to]
+        reply_to=reply_to
     )
     msg.attach_alternative(html_message, 'text/html')
     print(msg.send())
