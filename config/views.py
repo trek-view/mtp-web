cc197b9530f6da79d8c46418fa8bb7535aa2e4bd
good-lancer <developer613@sourceplay.com>
2020-10-27 17:33:22 +0000

    Index page should resolve too map photo list view #244

--- config/views.py
+++ config/views.py
@@ -6,13 +6,14 @@ from lib.functions import *
 MAIN_PAGE_DESCRIPTION = "Access street-level imagery and map data from all over the world.  Fill in the gaps by requesting new coverage or capturing your own."
 
 def index(request):
-    # content = {
-    #     'pageName': 'Map the Paths',
-    #     'pageTitle': 'Map the Paths',
-    #     'pageDescription': MAIN_PAGE_DESCRIPTION
-    # }
-    # return render(request, 'home.html', content)
-    return redirect(reverse('sequence.sequence_list') + '?view_type=map-view')
+    content = {
+        'pageName': 'Map the Paths',
+        'pageTitle': 'Map the Paths',
+        'pageDescription': MAIN_PAGE_DESCRIPTION
+    }
+    return render(request, 'home.html', content)
+    # return redirect(reverse('sequence.sequence_list') + '?view_type=map-view')
+
 
 @my_login_required
 def app_download(request):
