b8ff6f3496b9b270a27dfe310cd8288e2aa123ff
good-lancer <developer613@sourceplay.com>
2020-10-30 09:40:08 +0000

    test for cron job

--- cron_job/management/commands/print_test.py
+++ cron_job/management/commands/print_test.py
@@ -1,13 +1,12 @@
 from django.core.management.base import BaseCommand
 from sequence.models import Sequence, Image, TransType
-
+import time
 
 
 class Command(BaseCommand):
     help = 'check sequences published on mapillary'
 
     def handle(self, *args, **options):
-        transport_types = TransType.objects.all()
-        print(transport_types.count())
-        for t in transport_types:
-            print(t.name)
+        for i in range(100):
+            print('---------------{}---------------'.format(i))
+            time.sleep(60)
