c78ea45aee924a91001a46eb745d0389309b0c2f
good-lancer <developer613@sourceplay.com>
2020-12-09 08:25:21 +0000

    change requirements.txt

--- config/settings/local.py
+++ config/settings/local.py
@@ -31,10 +31,10 @@ SECURE_SSL_REDIRECT = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
         'PORT': '5432'
     }
 }
