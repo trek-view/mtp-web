1fdc3c52bf836bc9397c48c47f588965829aa971
good-lancer <developer613@sourceplay.com>
2020-12-15 22:15:43 +0000

    test my sequence issue

--- sequence/admin.py
+++ sequence/admin.py
@@ -93,8 +93,7 @@ class SequenceAdmin(tags_input_admin.TagsInputAdmin):
         return obj.username
 
     def get_form(self, request, obj=None, **kwargs):
-        if obj.type == "1":
-            self.exclude = ("geometry_coordinates", 'geometry_coordinates_ary', 'coordinates_cas', 'coordinates_image', )
+        self.exclude = ("geometry_coordinates", 'geometry_coordinates_ary', 'coordinates_cas', 'coordinates_image', )
         form = super(SequenceAdmin, self).get_form(request, obj, **kwargs)
         return form
 
