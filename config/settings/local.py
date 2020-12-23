bc7a0410d33e530aee50e6f44fb1af2c46f6e34e
good-lancer <developer613@sourceplay.com>
2020-12-23 21:17:18 +0000

    Revert "file  overwrite true"
    This reverts commit 4b852f79b2ca2d994649b81d0bd7f495815876a8.

--- config/settings/local.py
+++ config/settings/local.py
@@ -83,7 +83,7 @@ LOGGING = {
 # Set S3 as the place to store your files.
 DEFAULT_FILE_STORAGE = "config.settings.storage_backends.MediaStorage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_S3_FILE_OVERWRITE = False
+AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = None
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY")
 AWS_ACCESS_KEY = AWS_ACCESS_KEY_ID
