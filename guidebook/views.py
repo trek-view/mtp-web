66dece64c298041f75e93b06bc6dc888b81f7566
good-lancer <developer613@sourceplay.com>
2020-12-24 11:38:15 +0000

    test deleting avatar

--- guidebook/views.py
+++ guidebook/views.py
@@ -138,8 +138,11 @@ def my_guidebook_list(request):
     from boto.s3.connection import S3Connection, Bucket, Key
     conn = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
     bucket = Bucket(conn, settings.AWS_S3_BUCKET_CNAME)
-    bucket.delete_key('user/avatar/superadmin')
+    # bucket.delete_key('user/avatar/superadmin')
 
+    user = request.user
+    user.avatar.delete()
+    user.save()
 
     global form
     guidebooks = None
